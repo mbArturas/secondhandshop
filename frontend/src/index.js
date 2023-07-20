ad72adc7f3e7cc26e7b0115dc1c21385e3ace267
Arturas <mbarturas@yahoo.com>
2023-07-21 00:23:06 +0300

    Rating Component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,11 +15,11 @@ import HomeScreen from './screens/HomeScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />} />
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
     </Route>
   )
-)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
