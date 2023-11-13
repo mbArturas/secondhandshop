eccf771638c896505400d9c985f963e1bc7f02b2
Arturas <mbarturas@yahoo.com>
2023-11-13 21:54:58 +0200

    upload product image - frontend

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -8,6 +8,7 @@ import { toast } from 'react-toastify';
 import {
   useGetProductDetailsQuery,
   useUpdateProductMutation,
+  useUploadProductImageMutation,
 } from '../../slices/productsApiSlice';
 
 const ProductEditScreen = () => {
@@ -30,6 +31,9 @@ const ProductEditScreen = () => {
 
   const [updateProduct, { isLoading: loadingUpdate }] =
     useUpdateProductMutation();
+
+  const [uploadProductImage, { isLoading: loadingUpload }] =
+    useUploadProductImageMutation();
 
   const navigate = useNavigate();
 
@@ -61,6 +65,18 @@ const ProductEditScreen = () => {
       toast.success('Product updated');
       refetch();
       navigate('/admin/productlist');
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append('image', e.target.files[0]);
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
     } catch (err) {
       toast.error(err?.data?.message || err.error);
     }
@@ -99,7 +115,7 @@ const ProductEditScreen = () => {
                 onChange={(e) => setPrice(e.target.value)}></Form.Control>
             </Form.Group>
 
-            {/* <Form.Group controlId='image'>
+            <Form.Group controlId='image'>
               <Form.Label>Image</Form.Label>
               <Form.Control
                 type='text'
@@ -111,7 +127,7 @@ const ProductEditScreen = () => {
                 onChange={uploadFileHandler}
                 type='file'></Form.Control>
               {loadingUpload && <Loader />}
-            </Form.Group> */}
+            </Form.Group>
 
             <Form.Group controlId='brand'>
               <Form.Label>Brand</Form.Label>
